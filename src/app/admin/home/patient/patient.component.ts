5bf5c530f281eb5a1e3a0343858b57dd7c34c91b
SelvaPraveen309 <101079687+SelvaPraveen309@users.noreply.github.com>
2023-03-15 19:37:58 +0530

    commit

---
+++ src/app/admin/home/patient/patient.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient',
+  templateUrl: './patient.component.html',
+  styleUrls: ['./patient.component.css']
+})
+export class PatientComponent {
+[x: string]: any;
+
+}
